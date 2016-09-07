3a62778df7281a981e804c39d5f36beffcd3c761
billbenetti <brunobillbenetti@gmail.com>
2016-09-07 09:33:16 -0300

    primeiroUpload

---
+++ temp1.py
@@ -0,0 +1,15 @@
+#print "------------------"
+#C = -20
+#dC = 5
+#while C <= 40:
+#	F = (9.0/5)*C + 32
+#	print C, F
+#	C = C + dC
+#print "------------------"
+
+degrees = [0, 10, 20, 40, 100]
+
+C = []
+for C in degrees:
+	print 'list element:', C
+print "The degrees list has", len(degrees), "elements"
